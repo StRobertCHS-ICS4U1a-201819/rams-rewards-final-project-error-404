051dc2fa44ae2ab70863707e77b7d24518738852
Dan Gallo <pokemonball34@gmail.com>
2018-12-20 14:31:48 +0000

    Create User class

--- RRSAsite/rewards/models.py
+++ RRSAsite/rewards/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class Reward(models.Model):
     reward_title = models.CharField(max_length=200)
@@ -7,3 +8,9 @@ class Reward(models.Model):
     
     def __str__(self):
         return self.reward_title
+
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+    
